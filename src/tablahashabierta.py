46ead8723106ceff9e7c48b5d5ebcb0e4602277b
prislq <priscilla.lopezquesada@ucr.ac.cr>
2025-10-02 22:49:08 -0600

    Inicio implementación Tabla Hash: capacidad, carga máxima y tamaño

--- src/tablahashabierta.py
+++ src/tablahashabierta.py
@@ -1,4 +1,13 @@
 from diccionario import Diccionario
+from listaordenadadinamica import ListaOrdenadaDinamica
 
 class TablaHashAbierta(Diccionario):
-    pass
+    
+    def __init__(self, capacidad=10, carga_max=0.7):
+        self.__capacidad = capacidad
+        self.__carga_max = carga_max
+        self.__tamanno = 0
+        self.__buckets = [ListaOrdenadaDinamica() for _ in range(capacidad)]#bucket es ubicación de memoria
+      
+    def __len__(self):
+        return self.__tamanno  
