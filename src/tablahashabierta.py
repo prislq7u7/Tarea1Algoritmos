07420768de4be1e087ef67599c76a55c000afa7f
prislq <priscilla.lopezquesada@ucr.ac.cr>
2025-10-03 00:37:57 -0600

    Función tabla hash para strings

--- src/tablahashabierta.py
+++ src/tablahashabierta.py
@@ -1,7 +1,7 @@
 from diccionario import Diccionario
-from listaordenadadinamica import ListaOrdenadaDinamica
+from listaordenadadinamica import ListaOrdenadaDinamica#cada bucket es una lista ordenada dinámica
 
-class TablaHashAbierta(Diccionario):
+class TablaHashAbierta(Diccionario):#herencia de Diccionario
     
     def __init__(self, capacidad=10, carga_max=0.7):
         self.__capacidad = capacidad
@@ -11,3 +11,9 @@ class TablaHashAbierta(Diccionario):
       
     def __len__(self):
         return self.__tamanno  
+    
+    def __funcion_hash(self, elemento: str) -> int:#función para strings
+        val_hash = 0
+        for char in elemento:
+            val_hash = (val_hash * 31 + ord(char)) % self.__capacidad
+        return val_hash
