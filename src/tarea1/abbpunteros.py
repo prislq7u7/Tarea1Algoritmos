b3e8a8ef5dee6b6cf2260db31aefdaf1eed47fa6
Raven7u7 <priscilla.lopezquesada@ucr.ac.cr>
2025-10-17 17:40:53 -0600

    Declaración de clase nodoABB

--- src/tarea1/abbpunteros.py
+++ src/tarea1/abbpunteros.py
@@ -1,4 +1,8 @@
 from tarea1.diccionario import Diccionario
 
-class AbbPunteros(Diccionario):
-    pass
+class Nodo ABB:
+    def __init__(self, elemento: str):
+        self.elemento = elemento#almacena el string
+        self.izquierdo = None#puntero al hijo izquierdo, pero inicia como None
+        self.derecho = None#puntero al hijo derecho, pero inicia como None
+        
