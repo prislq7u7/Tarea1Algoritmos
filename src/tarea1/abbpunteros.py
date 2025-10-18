627b6bcb1af0e0ddb68959c94618b17f738ade72
Raven7u7 <priscilla.lopezquesada@ucr.ac.cr>
2025-10-17 18:03:56 -0600

    declaración de clase ABBPunteros y corrección de nodoabb porque lo escribí separado jeje

--- src/tarea1/abbpunteros.py
+++ src/tarea1/abbpunteros.py
@@ -1,8 +1,13 @@
 from tarea1.diccionario import Diccionario
 
-class Nodo ABB:
+class NodoABB:
     def __init__(self, elemento: str):
         self.elemento = elemento#almacena el string
         self.izquierdo = None#puntero al hijo izquierdo, pero inicia como None
         self.derecho = None#puntero al hijo derecho, pero inicia como None
+        
+class ABBPunteros(Diccionario):
+    def __init__(self):
+        self.__raiz: NodoABB | None = None#puntero a la raiz, inicia como None
+        self.__tamanno = 0#contador de elementos en el árbol
         
